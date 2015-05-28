b2fcf6cfa81492922763d25c1ca1ef8df52e4bb4
keiya <keiya_21@yahoo.co.jp>
2015-05-28 17:31:02 +0900

    some fix

--- tools/bootconf.sh
+++ tools/bootconf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo mv bzImage /boot/vmlinuz-custom
-sudo mv System.map /boot/System.map-custom
+sudo mv ~/bzImage /boot/vmlinuz-custom
+sudo mv ~/System.map /boot/System.map-custom
 sudo update-initramfs -c -k custom
 sudo update-grub
 
