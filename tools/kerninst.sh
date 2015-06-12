11266ffee0c0080a2f163c2d83edc3b17a84288c
keiya <keiya_21@yahoo.co.jp>
2015-06-12 17:36:57 +0900

    fix

--- tools/kerninst.sh
+++ tools/kerninst.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-cd ~/linux-4.0.1
+LINUXBASE="$HOME/linux-4.0.5"
+
+cd $LINUXBASE
 cp ~/Documents/kernelhack/config_template .config
 
-make -j2 && make modules && scp -c arcfour ~/linux-4.0.1/System.map ~/linux-4.0.1/arch/x86_64/boot/bzImage user@172.16.5.129:~
+make -j2 && make modules && scp -c arcfour $LINUXBASE/System.map $LINUXBASE/arch/x86_64/boot/bzImage user@172.16.5.129:~
