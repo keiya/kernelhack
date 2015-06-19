c6154884f37b5d5d42106ccd250dbefcc55f8d05
keiya <keiya_21@yahoo.co.jp>
2015-06-19 15:04:34 +0900

    let's encrypt

---
+++ myvpn/evp_gcm.h
@@ -0,0 +1,7 @@
+void handleErrors(void);
+int encrypt(unsigned char *, int, unsigned char *,
+    int, unsigned char *, unsigned char *,
+    unsigned char *, unsigned char *);
+int decrypt(unsigned char *, int, unsigned char *,
+    int, unsigned char *, unsigned char *, unsigned char *,
+    unsigned char *);
